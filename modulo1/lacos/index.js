69f3389de797be07bd5d8b61cb907783fc45491f
unknown <yasminsilvaconc@gmail.com>
2022-02-23 17:29:18 -0300

    laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,98 @@
+//Exercícios de interpretação de código
+
+//Exercício1
+
+//Ele está somando os valores de 1 em 1 até chegar ao número 4. resultado é 10.
+
+
+//Exercício2
+
+//A) 19,21,23,25,27,30
+//B) sim é suficiente. poderia ser usado dessa forma:  if (numero >= 10)
+
+
+//Exercício3
+
+//seriam 4 linhas de asteristicos
+
+
+
+//Exercícios de escrita de código
+
+//Exercício1
+
+// let bichinhosTem = Number(prompt("Quantos bichinhos você tem?"))
+// if (bichinhosTem === 0 ) {
+//     console.log("Que pena! Você pode adotar um pet!")
+
+// }else{
+//     array = []
+//     for(let i = 0; i < bichinhosTem; i++){
+//         let nomePet = prompt("Digite o nome dos seus pets:")
+//         array.push(nomePet)
+//     } 
+
+
+//        console.log(array)
+// }
+
+
+//Exercício2
+
+//A) 
+// function primeiraFuncao(array) {
+//     for (let i = 0; i < array.length; i++)
+//     console.log(array)
+// }
+
+//B)
+// function segundaFuncao(array) {
+   
+//     for (let i = 0; i < 5; i ++){
+//     resultado = array[i]/10
+//     console.log(resultado)
+//     }
+
+// }
+
+//C) 
+// function terceiraFuncao(array) {
+//     let array1 = []
+//     for (let i = 0; i < array.length; i++){
+//     if (array[i] % 2 === 0 ){
+//       array1.push (array[i])
+//         }
+//     }
+//     console.log(array1);
+// }
+
+//D) 
+// function quartaFuncao(array) {
+//     let array = []
+//     for (let i = 0; i < array.lenght; i++){
+//     array.push (`O elemento do index ${i} é: ${array}`)
+
+//     }
+// console.log(array)
+
+// }
+
+//E)
+// function descobreMaiorEMenor(array) {
+//    let maior = array[0] 
+//    let menor = array [0]
+
+//     for(number of array){
+
+//       if (number > maior){
+//           maior = number
+//       }
+//       if (number < menor){
+//           menor = number
+//       }
+//     }
+//     console.log(maior)
+//     console.log(menor)
+// }
+
+// descobreMaiorEMenor(arrayOriginal)
