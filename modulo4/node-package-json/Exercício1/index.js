dd6df5a22318a30b8d18360c34c15eef50e15717
yasminconc <yasminsilvaconc@gmail.com>
2022-05-24 14:43:59 -0300

    node-package-json

---
+++ modulo4/node-package-json/Exercício1/index.js
@@ -0,0 +1,9 @@
+//1) A = Pode ser acessado pelo process.argv
+
+//B) 
+
+const nome = "yasmin"
+const idade = 19
+const novaIdade = Number(idade) + 7
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}`)
