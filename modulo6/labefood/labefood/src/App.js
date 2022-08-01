4851c3c132cdd8f434ec0bfa60fba910533ebb60
yasminconc <yasminsilvaconc@gmail.com>
2022-08-01 14:19:15 -0300

    projeto-cancelado

--- modulo6/labefood/labefood/src/App.js
+++
@@ -1,9 +0,0 @@
-import Router from "./Routes/Router";
-
-export default function App() {
-  return(
-    <div>
-      <Router />
-    </div>
-  )
-}
--- modulo6/labefood/labefood/src/Pages/SignupAdress/SignupAdress.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function SignupAdress () {
-    return(
-        <div>SignupAdress</div>
-    )
-}
